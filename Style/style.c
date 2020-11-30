36c55e5aa63270ccdb96ce5fa299b98b2c085e8b
ahui <843219581@qq.com>
2020-11-30 09:28:44 +0800

    create a repo, and add the c style file.

---
+++ Style/style.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+
+int leap_year(int year);
+
+
+
+
+int main() {
+    /*
+    *traverse the array
+    int nelems = 10;
+    int elem[10];
+    for (int i = 0; i < nelems; i++) {
+        elem[i] = i % 2 == 0? i*i : i*i*i;
+    }
+    for (int i = 0; i < nelems; i++) {
+        printf("%d ", elem[i]);
+    }
+    printf("\n");
+    */
+    /*
+    *is a leap year?
+    int year = 2020;
+    printf("%d %s a leap year.\n", year, leap_year(year) ? "is" : "is not");
+    */
+    
+    return 0;
+}
+ 
+int leap_year(int year) {
+    return ((year % 4 == 0) && (year % 100 != 0) || (year % 400) == 0);
+}
